92904c3bead0ca838eefee49e778a735c6153900
zbt <1132896131@qq.com>
2024-09-30 23:06:41 +0800

    wanchengzuoye

--- main.cpp
+++ main.cpp
@@ -3,9 +3,29 @@
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, 4& 6)
 {
-    // 你的算法
+#include <stdio.h>
+	int a, b, c;
+	int max1(int x, int y);
+	int main()
+	{
+		scanf("%d %d %d", &a, &b, &c);
+		int n = max1(a, b);
+		printf("%d", max1(n, c));
+		return 0;
+	}
 
-    result.min = 最小值;
-    result.mid = 中间值;
-    result.max = 最大值;
+	int max1(int x, int y)
+	{
+		int z;
+		if (x > y)
+			z = x;
+		else
+			z = y;
+		return z;
+	}
+
+
+	result.min = a;
+    result.mid = b;
+    result.max = c;
 }
